1fbe75504a917fcba5556702fe08582bc39a43e4
martin <msterlicchi@gmail.com>
2020-04-25 23:24:16 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var dicebutton = document.getElementsByClassName("diceButton")[0];
 dicebutton.onclick = function diceroll() {
   var dice1 = document.querySelector("img.img1");
-  var randomNumber1 = Math.ceil(Math.random() * 6);
+  var randomNumber1 = Math.floor(Math.random() * 6 + 1);
   var image1 = "images/dice" + randomNumber1 + ".png";
   dice1.setAttribute("src", image1);
   var dice2 = document.querySelector("img.img2");
-  var randomNumber2 = Math.ceil(Math.random() * 6);
+  var randomNumber2 = Math.floor(Math.random() * 6 + 1);
   var image2 = "images/dice" + randomNumber2 + ".png";
   dice2.setAttribute("src", image2);
   var victoryText = document.querySelector("h1");
